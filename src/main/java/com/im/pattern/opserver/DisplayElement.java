1f4c9f1124881ffd0e1ee3c64840730465a5bc04
sungbo <ishimsungbo@gmail.com>
2022-05-14 18:22:58 +0900

    옵저버패턴

---
+++ src/main/java/com/im/pattern/opserver/DisplayElement.java
@@ -0,0 +1,5 @@
+package com.im.pattern.opserver;
+
+public interface DisplayElement {
+    public void display();
+}
---
+++ src/main/java/com/im/pattern/opserver/Observer.java
@@ -0,0 +1,5 @@
+package com.im.pattern.opserver;
+
+public interface Observer {
+    public void update(float temp, float humidity, float pressure);
+}
